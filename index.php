a933ceb6d6322d2d630861a2b84ef937f4183074
JD17VM <jdiegodiego4@gmail.com>
2024-04-17 10:57:01 -0500

    Se siguen borrando partes innecesarias

--- index.php
+++ index.php
@@ -14,99 +14,6 @@
         content="jpawaj.com:: jpawa :: jjpawa :: tecnico :: COMPUTADORAS :: DELIBERY laptos AREQUIPA :: AQP :: tintas arequipa delivery :: delibears peru :: Delibears Arequipa Perú :: syscomputer :: mujer bonita :: Boutique :: mujer bonita boutique" />
     <title>Jpawaj SAC</title>
 
-
-    <style type="text/css">
-        .PRECIOFERTA {
-            color: #009;
-            font-weight: bold;
-            font-size: 20px;
-        }
-
-        .TELEFONOS {
-            color: #009;
-            font-weight: bold;
-            font-size: 30px;
-            font-style: italic;
-
-        }
-
-        .PRODUCTOTXT {
-            font-weight: bold;
-            font-size: 10px;
-        }
-
-        .div_productos {
-            /* background-color:var(--azul_principal); */
-            /* background-color:#FFFFC4;  */
-            height: 70vh;
-            display: flex;
-            flex-wrap: wrap;
-            align-items: center;
-            padding: 1rem;
-            justify-content: space-around;
-            /*  justify-content:center; */
-        }
-
-        .prod_contenedor {
-
-            /*  width:1rem; */
-
-        }
-
-        .contorno_bot_celular {
-            display: flex;
-            justify-content: center;
-            margin-top: 1rem;
-        }
-
-        .cabecera_principal img {
-
-            width: 100%;
-            height: auto;
-
-            /*with:50%; */
-            /*height:200px; */
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            /*padding:1rem; */
-            overflow: hidden;
-
-            padding-top: 6rem;
-        }
-
-
-
-        @media screen and (max-width: 700px) {
-            .div_productos {
-                height: 180vh;
-            }
-        }
-
-        .logo_new {
-
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            padding: 1rem;
-            overflow: hidden;
-            padding-top: 2rem;
-        }
-
-        .logo_new>img {
-            height: 80px;
-
-            /* height:60%; */
-            margin-right: 0.5rem;
-            /* max-height:60px; */
-        }
-
-        .tachado {
-            text-decoration: line-through;
-            color: red;
-            font-size: 12px;
-        }
-    </style>
 </head>
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-RMB75PC13F"></script>
@@ -131,11 +38,6 @@
     $xgl = $_GET['xgl'];
     ?>
 
-
-
-    <div class="logo_new">
-        <img src="imagenes/logo_SyscomputerCorp_ok_png.png" alt="logo_quienessomos">
-    </div>
 
     <!--  <div class="campo_formulario_prod_nuevo"> -->
     <div class="div_productos">
@@ -175,7 +77,6 @@
                                     <span class="PRECIOFERTA">
                                         <? echo ($simbolo_mone . money_format('%n', ($tabla["pv03_it"]))) ?>
                                     </span>
-
                                 </td>
                             </tr>
                         </table>
@@ -203,7 +104,6 @@
                                 </td>
                             </tr>
                         </table>
-
                     </td>
                 </tr>
                 <tr>
@@ -230,7 +130,6 @@
                                 </td>
                             </tr>
                         </table>
-
                     </td>
                     <td align="center">
                         <table width="186" border="0" cellspacing="0" cellpadding="0">
@@ -255,7 +154,6 @@
                                 </td>
                             </tr>
                         </table>
-
                     </td>
                 </tr>
             </table>
@@ -309,10 +207,7 @@
                                                 <div class="contorno_bot_celular">
                                                     <input type="submit" value="Enviar">
                                                 </div>
-
                                             </form>
-
-
                                         </td>
                                     </tr>
                                 </table>
@@ -323,30 +218,6 @@
                 </div>
 
             </div>
-        </div>
-        <div style=" display:flex; justify-content:flex-end;">
-            <p><a href="https://api.whatsapp.com/send?phone=51959956000&text=Más información" target="_blank"><img
-                        src="imagenes/b_whats_consulta1.png" onmouseover="this.src='imagenes/b_whats_consulta2.png'"
-                        onmouseout="this.src='imagenes/b_whats_consulta1.png'" /></a></p>
-        </div>
-
-        <div class="div_texto_producto">
-            <div class="parte_texto_new">
-                <p>&nbsp;</p>
-
-            </div>
-
-
-
-        </div>
-
-        <!--  MUJER BONITA AREA 02 "OFERTAS DE LA SEMANA   -->
-        <a name="xmujerbonita" id="xmujerbonita"></a>
-
-        <div class="linea_division"> </div>
-
-        <div class="logo_new">
-            <img src="imagenes/logo_mujerbonita_boutique_ok_png.png" alt="logo_quienessomos">
         </div>
 
         <!--  <div class="campo_formulario_prod_nuevo"> -->
@@ -414,7 +285,6 @@
                                     </td>
                                 </tr>
                             </table>
-
                         </td>
                     </tr>
                     <tr>
@@ -466,7 +336,6 @@
                                     </td>
                                 </tr>
                             </table>
-
                         </td>
                     </tr>
                 </table>
@@ -474,89 +343,8 @@
 
             <!--  SYSCOMPUTER AREA 02 "COMUNICATE CON NOSOTROS  -->
 
-            <div class="formulario_prod_nuevo">
-                <div class="imagen_prod_nuevo">
-                    <div class="prod_contenedor">
-
-                        <table width="398" height="499" border="0" cellpadding="0" cellspacing="0">
-                            <tr>
-                                <td width="200" height="168" align="center">
-                                    <a href="siga_jpawaj/a_lisgeneral.php?xgl=M"><img src="imagenes/bmujerb_lista.jpg"
-                                            width="180" height="160" style="border:0;"
-                                            onmouseover="this.style.border='solid  2px  #0a3d89';"
-                                            onmouseout="this.style.border=0;" border="0"></a>
-                                </td>
-                                <td width="198" align="center"><img src="imagenes/bsys_ofecemos.jpg" width="182"
-                                        height="162"></td>
-                            </tr>
-                            <tr>
-                                <td height="168" align="center"><a href="siga_jpawaj/a_lisimagenes.php?xgl=M"><img
-                                            src="imagenes/bmujerb_oferta.jpg" width="180" height="160" style="border:0;"
-                                            onmouseover="this.style.border='solid  2px  #0a3d89';"
-                                            onmouseout="this.style.border=0;" border="0"></a> </td>
-                                <td align="center"><a href="https://www.facebook.com/mujerbonita.mb00/"><img
-                                            src="imagenes/bmujerb_face.jpg" width="180" height="160" style="border:0;"
-                                            onmouseover="this.style.border='solid  2px  #0a3d89';"
-                                            onmouseout="this.style.border=0;" border="0"></a> </td>
-                            </tr>
-                            <tr>
-                                <td height="63" align="center"><a
-                                        href="siga_jpawaj/a_lisgeneral.php?bxproducto=vestido&Submit3=-%3E+Buscar+%3C-"><img
-                                            src="imagenes/bmujerb_masv.jpg" width="180" height="160" style="border:0;"
-                                            onmouseover="this.style.border='solid  2px  #0a3d89';"
-                                            onmouseout="this.style.border=0;" border="0"></a>
-                                </td>
-                                <td align="center">
-                                    <table width="100" border="0" cellspacing="0" cellpadding="0">
-                                        <tr>
-                                            <td><img src="imagenes/bsys_disponible.jpg" width="184" height="65"></td>
-                                        </tr>
-                                        <tr>
-                                            <td width="120" height="96" align="center" bgcolor="#CCCCCC">
-                                                <form class="formulario_nohay" method="post" action="insert_mail.php">
-                                                    <div class="campos_formulario">
-                                                        <!--  <label  class="label_form">Su Número de Celular</label> -->
-                                                        <input type="text" id="xcelular" name="xcelular"
-                                                            placeholder="N° Celular">
-                                                        <input type="hidden" name="xarea" value="Syscomputer: ">
-                                                    </div>
-                                                    <div class="contorno_bot_celular">
-                                                        <input type="submit" value="Enviar">
-                                                    </div>
-
-                                                </form>
-
-
-                                            </td>
-                                        </tr>
-                                    </table>
-                                </td>
-                            </tr>
-                        </table>
-
-                    </div>
-
-                </div>
-            </div>
-
-
-            <!-- FFFFIIIINNNN *****    01A   **************************** -->
-
-            <!-- DIV PRODUCTOS ******   01B   *************************** -->
-            <a name="xproductos" id="xproductos"></a>
-
-            <div class="linea_division"> </div>
-
-
-            <div class="titular_con_logo_prod_new">
-                <img src="imagenes/titico_productos.png" alt="logo_quienessomos">
-
-            </div>
-
-
 
             <div class="campo_formulario_prod_nuevo">
-
                 <div class="imagen_prod_nuevo">
                     <img src="imagenes/f_productos.jpg" alt="logo_quienessomos">
                 </div>
@@ -564,7 +352,6 @@
                     <div class="parte_formulario">
                         <div class="contenedor_formulario">
                             <h2 class="cabecera_formulario">Buenos productos.<br>Necesito más información.</h2>
-
                             <form class="formulario_oficial" method="post" action="insert_mail.php">
                                 <div class="campos_formulario">
                                     <label class="label_form"> Que producto? - Mensaje:</label>
@@ -586,36 +373,13 @@
                                     <input type="submit" value="Enviar">
                                 </div>
                                 <h2 class="pie_formulario">Responderemos inmediatamente.<br> Gracias.</h2>
-
                             </form>
-
-
                         </div>
                     </div>
                 </div>
             </div>
 
-
-
-            <a name="xinterclass" id="xinterclass"></a>
-            <div class="linea_division"> </div>
-            <div class="logo_new">
-                <img src="imagenes/logo_interclass_png.png" alt="logo_quienessomos">
-            </div>
             <div class="campo_formulario_nuevo campo_formulario_d">
-
-                <div class="imagen_nuevo">
-
-                    <img src="imagenes/aviso_curso_web.jpg" alt="logo_quienessomos">
-
-                </div>
-
-                <div class="imagen_nuevo">
-
-                    <img src="imagenes/f_contenido_excel_2024.jpg" alt="logo_quienessomos">
-
-                </div>
-
                 <div class="imagen_nuevo">
                     <!-- <div class="formulario_nuevo"> -->
                     <div class="parte_formulario">
@@ -646,62 +410,7 @@
                                     <input type="submit" value="Enviar">
                                 </div>
                                 <h2 class="pie_formulario">Responderemos inmediatamente.<br> Gracias.</h2>
-
                             </form>
-
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-            <!-- DIV STECNICO ********************************* -->
-            <a name="xstecnico" id="xstecnico"></a>
-
-            <div class="linea_division"> </div>
-
-
-            <div class="titular_con_logo_stec_new">
-                <img src="imagenes/titico_stecnico.png" alt="logo_quienessomos">
-
-            </div>
-
-
-            <div class="campo_formulario_nuevo">
-
-                <div class="imagen_stec_nuevo">
-                    <img src="imagenes/f_stecnico.png" alt="logo_quienessomos">
-                </div>
-                <div class="formulario_stec_nuevo">
-                    <div class="parte_formulario">
-                        <div class="contenedor_formulario">
-                            <h2 class="cabecera_formulario">Deseo obtener una<br> revisión GRATUITA!<br> de mi equipo.
-                                Gracias</h2>
-
-                            <form class="formulario_oficial" method="post" action="insert_mail.php">
-                                <div class="campos_formulario">
-                                    <label class="label_form"> Mi equipo es: PC, Laptop, Impresora, otro?:</label>
-                                    <input type="text" id="xmensaje" name="xmensaje" placeholder="ej. Laptop">
-                                </div>
-
-                                <div class="campos_formulario">
-                                    <label class="label_form">Descripción de la falla:</label>
-                                    <input type="text" id="correo" name="correo"
-                                        placeholder="ej. Enciende. Pero... se queda en un cursor">
-                                </div>
-
-                                <div class="campos_formulario">
-                                    <label class="label_form">IMPORTANTE! Mi número telefónico:</label>
-                                    <input type="text" id="telefono" name="telefono" placeholder="ej. 987458647">
-                                </div>
-                                <input type="hidden" name="xarea" value="STECNICO">
-                                <div class="contorno_boton">
-                                    <input type="submit" value="Enviar">
-                                </div>
-                                <h2 class="pie_formulario">Responderemos inmediatamente.<br> Gracias.</h2>
-                            </form>
-
                         </div>
                     </div>
                 </div>
