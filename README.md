ee7d3551a6ccee80fb44caee3aeb9d96865f82eb
JD17VM <jdiegodiego4@gmail.com>
2024-04-18 12:59:59 -0500

    Probando

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 ## Evitar los siguientes errores
 
+1. Llamar a las primeras filas (registros) de una tabla. 
+
+``` php
+//PRIMER BLOQUE
+<? $tabla = mysqli_fetch_array($result);
+    $producto_it = $tabla["producto_it"]; ?>
+
+<img src="<? echo "siga_jpawaj/img_items/" . $tabla["img_it"]; ?>" width="130" height="%" />
+
+<? echo ($simbolo_mone . ($tabla["pv01_it"])) ?>
+<? echo ($simbolo_mone . ($tabla["pv03_it"])) ?>
+
+
+//SEGUNDO BLOQUE
+<? $tabla = mysqli_fetch_array($result);
+    $producto_it = $tabla["producto_it"]; ?>
+
+<img src="<? echo "siga_jpawaj/img_items/" . $tabla["img_it"]; ?>" width="130" height="%" />
+
+<? echo ($simbolo_mone . ($tabla["pv01_it"])) ?>
+<? echo ($simbolo_mone . ($tabla["pv03_it"])) ?>
+```
+
+    - En este caso se pueden ver 2 bloques de codigo exactamente iguales, sin embargo no van a mostrar lo mismo. Ya que al llamar a `mysql_fetch_array()`, la primera vez mostrará le primer registro, si lo volvemos a llmar ya no mostrará el primer registro, sino el segundo registro y así sucesivamente. Sin embargo no es una buena practica recomendada
+
+Lo correcto sería usar un bucle para mostrar estos registros
+
+```php
+while ($tabla = mysqli_fetch_array($result)) {
+        $producto_it = $tabla["producto_it"];
+        echo $producto_it;
+?>
+        <img src="<?php echo "siga_jpawaj/img_items/" . $tabla["img_it"]; ?>" width="130" height="%" />
+        <?php echo ($simbolo_mone . ($tabla["pv01_it"])) ?>
+        <?php echo ($simbolo_mone . ($tabla["pv03_it"])) ?>
+        <br>
+<?php
+```
--- index_corregido.php
+++ index_corregido.php
@@ -1,41 +1,55 @@
+<?php
+include ("connec_sql_new.php");
+mysqli_set_charset($connec, 'utf8');
+date_default_timezone_set("America/Lima");
+setlocale(LC_ALL, "sp");
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="assets/css/estilos_index/estilos_generales.css">
     <link rel="stylesheet" href="assets/css/estilos_index/estilos_navegador.css">
     <link rel="stylesheet" href="assets/css/estilos_index/estilos_footer.css">
     <link rel="stylesheet" href="assets/css/estilos_index/estilos_slider_imagenes.css">
-        <link rel="stylesheet" href="assets/css/estilos_index/estilos_bloques_marcas/estilos_bloques_marcas_general.css">
-        <link rel="stylesheet" href="assets/css/estilos_index/estilos_bloques_marcas/estilos_contenedor_ofertas_semana.css">
-        <link rel="stylesheet" href="assets/css/estilos_index/estilos_bloques_marcas/estilos_contenedor_imagenes_grid.css">
-        <link rel="stylesheet" href="assets/css/estilos_index/estilos_bloques_marcas/estilos_cotenedor_quienes_somos.css">
+    <link rel="stylesheet" href="assets/css/estilos_index/estilos_bloques_marcas/estilos_bloques_marcas_general.css">
+    <link rel="stylesheet" href="assets/css/estilos_index/estilos_bloques_marcas/estilos_contenedor_ofertas_semana.css">
+    <link rel="stylesheet" href="assets/css/estilos_index/estilos_bloques_marcas/estilos_contenedor_imagenes_grid.css">
+    <link rel="stylesheet" href="assets/css/estilos_index/estilos_bloques_marcas/estilos_cotenedor_quienes_somos.css">
     <link rel="stylesheet" href="assets/css/estilos_index/estilos_contenedor_marcas.css">
     <link rel="stylesheet" href="assets/css/estilos_index/estilos_servicio_tecnico.css">
     <link rel="stylesheet" href="assets/css/estilos_index/estilos_bloque_marca_interclass.css">
     <title>JPAWAJ</title>
     <link rel="icon" href="assets/img/imagenes_index/icono_pestana/icono_jpawaj.png" type="image/png">
-</head> 
+</head>
+
 <body>
     <div class="navegador">
         <img src="assets/img/imagenes_index/logo_JPAWAJ_rojo_blanco.png" alt="" class="logo_principal">
-        
-        <button class="boton_nav" id="boton_hamburguesa"  onclick="desplegarBotonHamburguesa()">
+
+        <button class="boton_nav" id="boton_hamburguesa" onclick="desplegarBotonHamburguesa()">
             <span class="material-symbols-outlined">
                 menu
-                </span>
+            </span>
         </button>
 
         <div class="cont_lista_elementos" id="cont_elementos_navegador">
-            <ul>    
-                <li><a href="#syscomputer"><img src="assets/img/imagenes_index/logo_syscomputer_blanco.png" alt=""></a></li>
-                <li><a href="#mujer_bonita"><img src="assets/img/imagenes_index/logo_mujer_bonita_blanco.png" alt=""></a></li>
+            <ul>
+                <li><a href="#syscomputer"><img src="assets/img/imagenes_index/logo_syscomputer_blanco.png" alt=""></a>
+                </li>
+                <li><a href="#mujer_bonita"><img src="assets/img/imagenes_index/logo_mujer_bonita_blanco.png"
+                            alt=""></a></li>
                 <li><a href="#"><img src="assets/img/imagenes_index/logo_mujer_bonita_blanco.png" alt=""></a></li>
-                <li><a href="#interclass"><img src="assets/img/imagenes_index/logo_interclass_blanco.png" alt=""></a></li>
+                <li><a href="#interclass"><img src="assets/img/imagenes_index/logo_interclass_blanco.png" alt=""></a>
+                </li>
             </ul>
         </div>
     </div>
@@ -45,17 +59,27 @@
     <div class="contenedor_slider_imagenes">
         <div class="slider-wrapper">
             <div class="slider">
-                <div class="imagen" id="slide-1" style="background-image: url('assets/img/imagenes_index/imagen_slider_1.jpg');"></div>
-                <div class="imagen" id="slide-2" style="background-image: url('assets/img/imagenes_index/imagen_slider_1.jpg');"></div>
-                <div class="imagen" id="slide-3" style="background-image: url('assets/img/imagenes_index/imagen_slider_1.jpg');"></div>
-                <div class="imagen" id="slide-4" style="background-image: url('assets/img/imagenes_index/imagen_slider_1.jpg');"></div>
-                <div class="imagen" id="slide-5" style="background-image: url('assets/img/imagenes_index/imagen_slider_1.jpg');"></div>
+                <div class="imagen" id="slide-1"
+                    style="background-image: url('assets/img/imagenes_index/imagen_slider_1.jpg');"></div>
+                <div class="imagen" id="slide-2"
+                    style="background-image: url('assets/img/imagenes_index/imagen_slider_1.jpg');"></div>
+                <div class="imagen" id="slide-3"
+                    style="background-image: url('assets/img/imagenes_index/imagen_slider_1.jpg');"></div>
+                <div class="imagen" id="slide-4"
+                    style="background-image: url('assets/img/imagenes_index/imagen_slider_1.jpg');"></div>
+                <div class="imagen" id="slide-5"
+                    style="background-image: url('assets/img/imagenes_index/imagen_slider_1.jpg');"></div>
             </div>
 
             <div class="slider-nav" id="slider-nav">
             </div>
         </div>
     </div>
+
+
+    <? $result = mysqli_query($connec, "select * from a_items where view03_it='S' AND grupolista_it like '%S%' order by producto_it");
+    $simbolo_mone = "S/ "; ?>
+
 
     <div class="bloque_marca bloque_marca_syscomputer" id="syscomputer">
         <img src="assets/img/imagenes_index/logo_syscomputer.png" alt="">
@@ -67,56 +91,29 @@
                 </span>
             </div>
             <div class="contenedor_productos">
-                
-
-
-                <div class="producto">
-                    <div class="contenedor_imagen"><img src="assets/img/imagenes_index/Medidas_proporciones/proporcion_1_1.png" alt=""></div>
-                    <h3>LAPTOP LENOVO AMD RYZEN 5 752U 2.8/4.3GHZ IDEAPAD 1 15AM 15.5” FHD...</h3>
-                    <div class="precios">
-                        <p class="precio_antiguo">S/. 1699.00</p>
-                        <p class="precio_oferta">S/. 1599.00</p>
+                <?php while ($tabla = mysqli_fetch_array($result)) { ?>
+                    <div class="producto">
+                        <div class="contenedor_imagen"><img
+                                src="<?php echo "siga_jpawaj/img_items/" . $tabla["img_it"]; ?>" /></div>
+                        <h3>
+                            <?php
+                            $producto_it = $tabla["producto_it"];
+                            echo $producto_it;
+                            ?>
+                        </h3>
+                        <div class="precios">
+                            <p class="precio_antiguo">
+                                <?php echo ($simbolo_mone . ($tabla["pv01_it"])) ?>
+                            </p>
+                            <p class="precio_oferta">
+                                <?php echo ($simbolo_mone . ($tabla["pv03_it"])) ?>
+                            </p>
+                        </div>
+                        <a href="">
+                            Comprar
+                        </a>
                     </div>
-                    <a href="">
-                        Comprar
-                    </a>
-                </div>
-
-                <div class="producto">
-                    <div class="contenedor_imagen"><img src="assets/img/imagenes_index/Medidas_proporciones/proporcion_1_1.png" alt=""></div>
-                    <h3>LAPTOP LENOVO AMD RYZEN 5 752U 2.8/4.3GHZ IDEAPAD 1 15AM 15.5” FHD...</h3>
-                    <div class="precios">
-                        <p class="precio_antiguo">S/. 1699.00</p>
-                        <p class="precio_oferta">S/. 1599.00</p>
-                    </div>
-                    <a href="">
-                        Comprar
-                    </a>
-                </div>
-
-                <div class="producto">
-                    <div class="contenedor_imagen"><img src="assets/img/imagenes_index/Medidas_proporciones/proporcion_1_1.png" alt=""></div>
-                    <h3>LAPTOP LENOVO AMD RYZEN 5 752U 2.8/4.3GHZ IDEAPAD 1 15AM 15.5” FHD...</h3>
-                    <div class="precios">
-                        <p class="precio_antiguo">S/. 1699.00</p>
-                        <p class="precio_oferta">S/. 1599.00</p>
-                    </div>
-                    <a href="">
-                        Comprar
-                    </a>
-                </div>
-
-                <div class="producto">
-                    <div class="contenedor_imagen"><img src="assets/img/imagenes_index/Medidas_proporciones/proporcion_1_1.png" alt=""></div>
-                    <h3>LAPTOP LENOVO AMD RYZEN 5 752U 2.8/4.3GHZ IDEAPAD 1 15AM 15.5” FHD...</h3>
-                    <div class="precios">
-                        <p class="precio_antiguo">S/. 1699.00</p>
-                        <p class="precio_oferta">S/. 1599.00</p>
-                    </div>
-                    <a href="">
-                        Comprar
-                    </a>
-                </div>
+                <?php } ?>
             </div>
         </div>
         <div class="contenedor_imagenes_grid grid_syscomputer">
@@ -135,11 +132,14 @@
             <div class="imagen_5">
                 <img src="assets/img/imagenes_index/Medidas_proporciones/proporcion_2_4.png" alt="">
             </div>
- 
+
         </div>
         <div class="contenedor_quienes_somos">
             <h3>¿Quienes somos?</h3>
-            <p>Somos una empresa comercializadora de productos generales y tecnológicos de calidad y garantía. Nuestra experiencia data de más de 25 años en el mercado PERUANO, siendo parte del Grupo SYSCOMPUTER. Contamos con una vitrina de productos de seguridad e informática. En esta situación de cuarentena activamos nuestro servicio DELIVERY. Gracias por su confianza y recuerde que trabajamos para usted.</p>
+            <p>Somos una empresa comercializadora de productos generales y tecnológicos de calidad y garantía. Nuestra
+                experiencia data de más de 25 años en el mercado PERUANO, siendo parte del Grupo SYSCOMPUTER. Contamos
+                con una vitrina de productos de seguridad e informática. En esta situación de cuarentena activamos
+                nuestro servicio DELIVERY. Gracias por su confianza y recuerde que trabajamos para usted.</p>
             <div class="contenedor_botones_de_contacto">
                 <a href="https://wa.me/+51959956000" target="_blank">
                     <p>Escribir por Whatsapp</p>
@@ -156,7 +156,7 @@
 
     <div class="division_secciones"></div>
 
-    <div class="bloque_marca bloque_marca_mujer_bonita"  id="mujer_bonita">
+    <div class="bloque_marca bloque_marca_mujer_bonita" id="mujer_bonita">
         <img src="assets/img/imagenes_index/logo_mujer_bonita.png" alt="">
         <div class="contenedor_ofertas_semana">
             <div class="header_ofertas">
@@ -167,7 +167,8 @@
             </div>
             <div class="contenedor_productos">
                 <div class="producto">
-                    <div class="contenedor_imagen"><img src="assets/img/imagenes_index/Medidas_proporciones/proporcion_1_1.png" alt=""></div>
+                    <div class="contenedor_imagen"><img
+                            src="assets/img/imagenes_index/Medidas_proporciones/proporcion_1_1.png" alt=""></div>
                     <h3>LAPTOP LENOVO AMD RYZEN 5 752U 2.8/4.3GHZ IDEAPAD 1 15AM 15.5” FHD...</h3>
                     <div class="precios">
                         <p class="precio_antiguo">S/. 1699.00</p>
@@ -179,7 +180,8 @@
                 </div>
 
                 <div class="producto">
-                    <div class="contenedor_imagen"><img src="assets/img/imagenes_index/Medidas_proporciones/proporcion_1_1.png" alt=""></div>
+                    <div class="contenedor_imagen"><img
+                            src="assets/img/imagenes_index/Medidas_proporciones/proporcion_1_1.png" alt=""></div>
                     <h3>LAPTOP LENOVO AMD RYZEN 5 752U 2.8/4.3GHZ IDEAPAD 1 15AM 15.5” FHD...</h3>
                     <div class="precios">
                         <p class="precio_antiguo">S/. 1699.00</p>
@@ -191,7 +193,8 @@
                 </div>
 
                 <div class="producto">
-                    <div class="contenedor_imagen"><img src="assets/img/imagenes_index/Medidas_proporciones/proporcion_1_1.png" alt=""></div>
+                    <div class="contenedor_imagen"><img
+                            src="assets/img/imagenes_index/Medidas_proporciones/proporcion_1_1.png" alt=""></div>
                     <h3>LAPTOP LENOVO AMD RYZEN 5 752U 2.8/4.3GHZ IDEAPAD 1 15AM 15.5” FHD...</h3>
                     <div class="precios">
                         <p class="precio_antiguo">S/. 1699.00</p>
@@ -203,7 +206,8 @@
                 </div>
 
                 <div class="producto">
-                    <div class="contenedor_imagen"><img src="assets/img/imagenes_index/Medidas_proporciones/proporcion_1_1.png" alt=""></div>
+                    <div class="contenedor_imagen"><img
+                            src="assets/img/imagenes_index/Medidas_proporciones/proporcion_1_1.png" alt=""></div>
                     <h3>LAPTOP LENOVO AMD RYZEN 5 752U 2.8/4.3GHZ IDEAPAD 1 15AM 15.5” FHD...</h3>
                     <div class="precios">
                         <p class="precio_antiguo">S/. 1699.00</p>
@@ -225,11 +229,14 @@
             <div class="imagen_8">
                 <img src="assets/img/imagenes_index/Medidas_proporciones/proporcion_6_3.png" alt="">
             </div>
- 
+
         </div>
         <div class="contenedor_quienes_somos">
             <h3>¿Quienes somos?</h3>
-            <p>Somos una empresa comercializadora de productos generales y tecnológicos de calidad y garantía. Nuestra experiencia data de más de 25 años en el mercado PERUANO, siendo parte del Grupo SYSCOMPUTER. Contamos con una vitrina de productos de seguridad e informática. En esta situación de cuarentena activamos nuestro servicio DELIVERY. Gracias por su confianza y recuerde que trabajamos para usted.</p>
+            <p>Somos una empresa comercializadora de productos generales y tecnológicos de calidad y garantía. Nuestra
+                experiencia data de más de 25 años en el mercado PERUANO, siendo parte del Grupo SYSCOMPUTER. Contamos
+                con una vitrina de productos de seguridad e informática. En esta situación de cuarentena activamos
+                nuestro servicio DELIVERY. Gracias por su confianza y recuerde que trabajamos para usted.</p>
             <div class="contenedor_botones_de_contacto">
                 <a href="https://wa.me/+51959956060" target="_blank">
                     <p>Escribir por Whatsapp</p>
@@ -262,7 +269,7 @@
                         <p>Escribir por Whatsapp</p>
                         <img src="assets/img/imagenes_index/logo_whatsapp_blanco.png" alt="">
                     </a>
-    
+
                     <a href="tel:+51959956000">
                         <p>Llamar por teléfono</p>
                         <img src="assets/img/imagenes_index/logo_llamada_negro.png" alt="">
@@ -329,17 +336,37 @@
         <h3>NUESTROS PRINCIPALES CLIENTES</h3>
         <div class="contenedor_slider">
             <div class="slider-track">
-                <div class="slide"><img src="assets/img/imagenes_index/Medidas_proporciones/Proporciones_marcas/marca_1_23_40.png" alt=""></div>
-                <div class="slide"><img src="assets/img/imagenes_index/Medidas_proporciones/Proporciones_marcas/marca_2_23_40.png" alt=""></div>
-                <div class="slide"><img src="assets/img/imagenes_index/Medidas_proporciones/Proporciones_marcas/marca_3_23_40.png" alt=""></div>
-                <div class="slide"><img src="assets/img/imagenes_index/Medidas_proporciones/Proporciones_marcas/marca_4_23_40.png" alt=""></div>
-                <div class="slide"><img src="assets/img/imagenes_index/Medidas_proporciones/Proporciones_marcas/marca_5_23_40.png" alt=""></div>
+                <div class="slide"><img
+                        src="assets/img/imagenes_index/Medidas_proporciones/Proporciones_marcas/marca_1_23_40.png"
+                        alt=""></div>
+                <div class="slide"><img
+                        src="assets/img/imagenes_index/Medidas_proporciones/Proporciones_marcas/marca_2_23_40.png"
+                        alt=""></div>
+                <div class="slide"><img
+                        src="assets/img/imagenes_index/Medidas_proporciones/Proporciones_marcas/marca_3_23_40.png"
+                        alt=""></div>
+                <div class="slide"><img
+                        src="assets/img/imagenes_index/Medidas_proporciones/Proporciones_marcas/marca_4_23_40.png"
+                        alt=""></div>
+                <div class="slide"><img
+                        src="assets/img/imagenes_index/Medidas_proporciones/Proporciones_marcas/marca_5_23_40.png"
+                        alt=""></div>
 
-                <div class="slide"><img src="assets/img/imagenes_index/Medidas_proporciones/Proporciones_marcas/marca_1_23_40.png" alt=""></div>
-                <div class="slide"><img src="assets/img/imagenes_index/Medidas_proporciones/Proporciones_marcas/marca_2_23_40.png" alt=""></div>
-                <div class="slide"><img src="assets/img/imagenes_index/Medidas_proporciones/Proporciones_marcas/marca_3_23_40.png" alt=""></div>
-                <div class="slide"><img src="assets/img/imagenes_index/Medidas_proporciones/Proporciones_marcas/marca_4_23_40.png" alt=""></div>
-                <div class="slide"><img src="assets/img/imagenes_index/Medidas_proporciones/Proporciones_marcas/marca_5_23_40.png" alt=""></div>
+                <div class="slide"><img
+                        src="assets/img/imagenes_index/Medidas_proporciones/Proporciones_marcas/marca_1_23_40.png"
+                        alt=""></div>
+                <div class="slide"><img
+                        src="assets/img/imagenes_index/Medidas_proporciones/Proporciones_marcas/marca_2_23_40.png"
+                        alt=""></div>
+                <div class="slide"><img
+                        src="assets/img/imagenes_index/Medidas_proporciones/Proporciones_marcas/marca_3_23_40.png"
+                        alt=""></div>
+                <div class="slide"><img
+                        src="assets/img/imagenes_index/Medidas_proporciones/Proporciones_marcas/marca_4_23_40.png"
+                        alt=""></div>
+                <div class="slide"><img
+                        src="assets/img/imagenes_index/Medidas_proporciones/Proporciones_marcas/marca_5_23_40.png"
+                        alt=""></div>
             </div>
         </div>
     </div>
@@ -380,7 +407,7 @@
                         <p>instagram/jpawaj</p>
                     </a>
                 </li>
-                
+
                 <li>
                     <a href="#">
                         <img src="assets/img/imagenes_index/logo_instagram_blanco.png" alt="">
@@ -412,4 +439,5 @@
     <script src="assets/js/navegador.js"></script>
     <script src="assets/js/slider_automatico.js"></script>
 </body>
+
 </html>
