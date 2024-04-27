09e1c008ce5a1fec0680f92049445283ae14dadb
JD17VM <jdiegodiego4@gmail.com>
2024-04-27 02:54:35 -0500

    Agregando correcion de enlaces footer y navegador 4

--- widgets/navegador.php
+++ widgets/navegador.php
@@ -1,7 +1,7 @@
-<link rel="stylesheet" href="<?php echo $agregado_en?>assets/css/estilos_navegador.css">
+<link rel="stylesheet" href="<?php echo $agregado_en_cab?>assets/css/estilos_navegador.css">
 
 <div class="navegador">
-        <img src="<?php echo $agregado_en?>assets/img/imagenes_index/logo_JPAWAJ_rojo_blanco.png" alt="" class="logo_principal">
+        <img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_JPAWAJ_rojo_blanco.png" alt="" class="logo_principal">
 
         <button class="boton_nav" id="boton_hamburguesa" onclick="desplegarBotonHamburguesa()">
             <span class="material-symbols-outlined">
@@ -11,10 +11,10 @@
 
         <div class="cont_lista_elementos" id="cont_elementos_navegador">
             <ul>
-                <li><a href="#syscomputer"><img src="<?php echo $agregado_en?>assets/img/imagenes_index/logo_syscomputer_blanco.png" alt=""></a></li>
-                <li><a href="#mujer_bonita"><img src="<?php echo $agregado_en?>assets/img/imagenes_index/logo_mujer_bonita_blanco.png"alt=""></a></li>
-                <li><a href="#"><img src="<?php echo $agregado_en?>assets/img/imagenes_index/logo_mujer_bonita_blanco.png" alt=""></a></li>
-                <li><a href="#interclass"><img src="<?php echo $agregado_en?>assets/img/imagenes_index/logo_interclass_blanco.png" alt=""></a>
+                <li><a href="#syscomputer"><img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_syscomputer_blanco.png" alt=""></a></li>
+                <li><a href="#mujer_bonita"><img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_mujer_bonita_blanco.png"alt=""></a></li>
+                <li><a href="#"><img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_mujer_bonita_blanco.png" alt=""></a></li>
+                <li><a href="#interclass"><img src="<?php echo $agregado_en_cab?>assets/img/imagenes_index/logo_interclass_blanco.png" alt=""></a>
                 </li>
             </ul>
         </div>
@@ -23,4 +23,4 @@
 <div class="navegador_espaciado"></div>
 
 
-<script src="<?php echo $agregado_en?>assets/js/navegador.js"></script>
+<script src="<?php echo $agregado_en_cab?>assets/js/navegador.js"></script>
