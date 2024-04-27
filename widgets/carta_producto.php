0ec6dc25a46d9a25bcc9810c36b698b9d5d7ccd1
JD17VM <jdiegodiego4@gmail.com>
2024-04-26 23:08:29 -0500

    .

--- widgets/carta_producto.php
+++ widgets/carta_producto.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="assets/css/estilos_carta_producto.css">
+<link rel="stylesheet" href="../assets/css/estilos_carta_producto.css">
 
 <div class="carta_producto">
     <div class="contenedor_imagen"><img src="<?php echo " siga_jpawaj/img_items/" . $tabla["img_it"];?>" /></div>
