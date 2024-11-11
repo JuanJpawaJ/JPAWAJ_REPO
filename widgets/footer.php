1d693db61f50a1e082b3355727a5dc4942c0b972
JD17VM <jdiegodiego4@gmail.com>
2024-11-11 00:14:25 -0500

    .

--- widgets/footer.php
+++ widgets/footer.php
@@ -63,4 +63,7 @@
         <img src="<?php echo $agregado_en?>assets/img/imagenes_index/logo_JPAWAJ_blanco.png" alt="">
         <p>Arequipa - Peru - 2024</p>
     </div>
+    <div>
+        Desarrollador: Juan Diego Valdivia Mendoza
+    </div>
 </div>
