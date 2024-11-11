deda4495b0d5c85bc643a638582dee291a2425fd
JD17VM <jdiegodiego4@gmail.com>
2024-11-10 21:59:40 -0500

    .

--- widgets/carta_producto.php
+++ widgets/carta_producto.php
@@ -1,6 +1,6 @@
 <!-- <link rel="stylesheet" href="../assets/css/estilos_carta_producto.css"> -->
 <?php
-    if ($view_03) {
+    if ($view_03 == 'S') {
         $clase_oferta = 'carta_producto_oferta';
         $texto_oferta = "<p>OFERTA</p>";
         $precio_antiguo = "<p class='precio_antiguo'>{$precio}</p>";
